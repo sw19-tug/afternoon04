1f5b5046efce5bfd0246841132b0e2f51c9b072b
Andreas Platzer <andreas.platzer@student.tugraz.at>
2019-03-22 17:47:18 +0100

    PAINT_007_001 Added Buttons to the dialog [MA, PA]

--- app/src/main/java/at/tugraz/ist/swe/MainActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/MainActivity.java
@@ -1,6 +1,7 @@
 package at.tugraz.ist.swe;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,6 +21,25 @@ public class MainActivity extends AppCompatActivity {
 
         AlertDialog.Builder bld_ColorPicker = new AlertDialog.Builder(this);
         bld_ColorPicker.setTitle(R.string.color_picker_title);
+        bld_ColorPicker.setNegativeButton(R.string.color_picker_button_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        bld_ColorPicker.setPositiveButton(R.string.color_picker_button_apply, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        bld_ColorPicker.setNeutralButton(R.string.color_picker_button_reset, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
         AlertDialog dlg_color = bld_ColorPicker.create();
         dlg_color.show();
     }
