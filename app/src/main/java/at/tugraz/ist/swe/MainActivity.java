877ea766d3fb43598af023e2d3520a3a65d1ee24
Anel <anel.memisevic@student.tugraz.at>
2019-03-22 18:35:53 +0100

    PAINT_007_001 Added 3 seekbars to the dialog [MA,PA]

--- app/src/main/java/at/tugraz/ist/swe/MainActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/MainActivity.java
@@ -1,12 +1,15 @@
 package at.tugraz.ist.swe;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,7 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AlertDialog.Builder bld_ColorPicker = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View dlg_view = inflater.inflate(R.layout.color_picker_dialog,null);
+
         bld_ColorPicker.setTitle(R.string.color_picker_title);
+        bld_ColorPicker.setView(dlg_view);
+
         bld_ColorPicker.setNegativeButton(R.string.color_picker_button_cancel, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
