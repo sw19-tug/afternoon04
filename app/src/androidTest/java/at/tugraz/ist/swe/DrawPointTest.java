074a54db2a14acf3b1dbc7b41e0c499225c81eb8
Hannes Unterweger <h.unterweger@student.tugraz.at>
2019-03-27 14:31:59 +0100

    PAINT_002 Added test for drawboard visibility [GR, UH]

---
+++ app/src/androidTest/java/at/tugraz/ist/swe/DrawPointTest.java
@@ -0,0 +1,27 @@
+package at.tugraz.ist.swe;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+@RunWith(AndroidJUnit4.class)
+public class DrawPointTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testDrawboardActivity()
+    {
+        //onView(withId(R.id.mainDrawboard)).check(matches(isDisplayed()));
+    }
+}
