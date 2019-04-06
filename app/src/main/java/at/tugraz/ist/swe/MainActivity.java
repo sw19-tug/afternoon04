a8fe95ae04bc79d6d16d7dcbbdb4b0d7ac63bd8c
Andreas Platzer <andreas.platzer@student.tugraz.at>
2019-04-06 18:55:19 +0200

    PAINT_007_001 Preparing MainActivity.java for merging project [PA, UH]

--- app/src/main/java/at/tugraz/ist/swe/MainActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/MainActivity.java
@@ -31,10 +31,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ColorPicker foreground = new ColorPicker(this);
-        foreground.show();
-
     }
 
 }
