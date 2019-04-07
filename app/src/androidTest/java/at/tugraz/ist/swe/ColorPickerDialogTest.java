ca16355a1eb87bddaaab0723dd147f51d756c02c
Andreas Platzer <andreas.platzer@student.tugraz.at>
2019-04-07 22:48:25 +0200

    PAINT_007_002 Fixed all Testcases. [PA]

--- app/src/androidTest/java/at/tugraz/ist/swe/ColorPickerDialogTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/ColorPickerDialogTest.java
@@ -1,14 +1,9 @@
 package at.tugraz.ist.swe;
 
-
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.test.espresso.Root;
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.action.MotionEvents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.MotionEvent;
 import android.widget.SeekBar;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.UiController;
@@ -25,21 +20,17 @@ import org.zakariya.flyoutmenu.FlyoutMenuView;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.RootMatchers.isDialog;
-import static android.support.test.espresso.matcher.RootMatchers.isTouchable;
 import static android.support.test.espresso.matcher.ViewMatchers.hasTextColor;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.AllOf.allOf;
 
 
 import android.support.test.espresso.matcher.ViewMatchers;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @RunWith(AndroidJUnit4.class)
 public class ColorPickerDialogTest {
@@ -47,32 +38,19 @@ public class ColorPickerDialogTest {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+
     @Test
     public void testDialogVisible()
     {
-        onView(withId(R.id.toolFlyoutMenu)).perform(click());
-        activityTestRule.getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                int amount_items = activityTestRule.getActivity().toolFlyoutMenu.getAdapter().getCount();
-                for(int counter = 0; 0 < amount_items; counter++)
-                {
-                    FlyoutMenuView.MenuItem result = activityTestRule.getActivity().toolFlyoutMenu.getAdapter().getItem(counter);
-                    if(((FlyoutToolbar.MenuItemImage)result).getID() == R.drawable.ic_outline_color_lens_24px)
-                    {
-                        activityTestRule.getActivity().toolFlyoutMenu.setSelectedMenuItem(result);
-                        break;
-                    }
-                }
-            }
-        });
+        openDialog();
         onView(withText(R.string.color_picker_title)).check(matches(isDisplayed()));
     }
 
-    /*
+
     @Test
     public void testDialogButtonsVisible()
     {
+        openDialog();
         onView(withText(R.string.color_picker_button_apply)).inRoot(isDialog()).check(matches(isDisplayed()));
         onView(withText(R.string.color_picker_button_reset)).inRoot(isDialog()).check(matches(isDisplayed()));
         onView(withText(R.string.color_picker_button_cancel)).inRoot(isDialog()).check(matches(isDisplayed()));
@@ -82,6 +60,7 @@ public class ColorPickerDialogTest {
     public void testDialogSliderVisible()
     {
         //SeekBar = Slider
+        openDialog();
         onView(withId(R.id.color_picker_seekbar_red)).inRoot(isDialog()).check(matches(isDisplayed()));
         onView(withId(R.id.color_picker_seekbar_green)).inRoot(isDialog()).check(matches(isDisplayed()));
         onView(withId(R.id.color_picker_seekbar_blue)).inRoot(isDialog()).check(matches(isDisplayed()));
@@ -89,6 +68,7 @@ public class ColorPickerDialogTest {
 
     @Test
     public void testButtonColors(){
+        openDialog();
         onView(withText("Cancel")).inRoot(isDialog()).check(matches(hasTextColor(R.color.colorCancelButtons)));
         onView(withText("Reset")).inRoot(isDialog()).check(matches(hasTextColor(R.color.colorResetButtons)));
         onView(withText("Apply")).inRoot(isDialog()).check(matches(hasTextColor(R.color.colorAcceptButtons)));
@@ -96,6 +76,7 @@ public class ColorPickerDialogTest {
 
     @Test
     public void testTextViewVisible(){
+        openDialog();
         onView(withId(R.id.textView_red_color)).inRoot(isDialog()).check(matches(isDisplayed()));
         onView(withId(R.id.textView_green_color)).inRoot(isDialog()).check(matches(isDisplayed()));
         onView(withId(R.id.textView_blue_color)).inRoot(isDialog()).check(matches(isDisplayed()));
@@ -103,6 +84,7 @@ public class ColorPickerDialogTest {
 
     @Test
     public void testRGBLabelsVisible(){
+        openDialog();
         onView(withId(R.id.color_picker_label_r)).inRoot(isDialog()).check(matches(isDisplayed()));
         onView(withId(R.id.color_picker_label_g)).inRoot(isDialog()).check(matches(isDisplayed()));
         onView(withId(R.id.color_picker_label_b)).inRoot(isDialog()).check(matches(isDisplayed()));
@@ -110,11 +92,13 @@ public class ColorPickerDialogTest {
 
     @Test
     public void testColorPreviewVisible(){
+        openDialog();
         onView(withId(R.id.color_picker_preview)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testTextBoxShowsSeekBarProgress() {
+        openDialog();
         onView(withId(R.id.color_picker_seekbar_red)).perform(setProgress(0));
         onView(withId(R.id.textView_red_color)).check(matches(withText("0")));
 
@@ -126,6 +110,7 @@ public class ColorPickerDialogTest {
     }
     @Test
     public void testCorrectColorPreview() {
+        openDialog();
         onView(withId(R.id.color_picker_seekbar_red)).perform(setProgress(0));
         onView(withId(R.id.color_picker_seekbar_green)).perform(setProgress(100));
         onView(withId(R.id.color_picker_seekbar_blue)).perform(setProgress(255));
@@ -136,12 +121,13 @@ public class ColorPickerDialogTest {
 
     @Test
     public void testResetButton() {
+        openDialog();
         onView(withId(R.id.color_picker_seekbar_red)).perform(setProgress(0));
         onView(withId(R.id.color_picker_seekbar_green)).perform(setProgress(100));
         onView(withId(R.id.color_picker_seekbar_blue)).perform(setProgress(255));
 
         onView(withText("Apply")).inRoot(isDialog()).perform(click());
-
+        openDialog();
         onView(withId(R.id.color_picker_seekbar_red)).perform(setProgress(200));
         onView(withId(R.id.color_picker_seekbar_green)).perform(setProgress(0));
         onView(withId(R.id.color_picker_seekbar_blue)).perform(setProgress(20));
@@ -154,24 +140,28 @@ public class ColorPickerDialogTest {
 
     }
 
+
     @Test
     public void testSeekbarShowsTextBoxValue() {
-        onView(withId(R.id.textView_red_color)).perform(setText("0"));
+        openDialog();
+        onView(withId(R.id.textView_red_color)).perform(replaceText("0"), pressImeActionButton());
         onView(withId(R.id.color_picker_seekbar_red)).check(matches(withProgress(0)));
-        onView(withId(R.id.textView_green_color)).perform(setText("100"));
+        onView(withId(R.id.textView_green_color)).perform(replaceText("100"), pressImeActionButton());
         onView(withId(R.id.color_picker_seekbar_green)).check(matches(withProgress(100)));
-        onView(withId(R.id.textView_blue_color)).perform(setText("256"));
+        onView(withId(R.id.textView_blue_color)).perform(replaceText("255"), pressImeActionButton());
         onView(withId(R.id.color_picker_seekbar_blue)).check(matches(withProgress(255)));
     }
 
     @Test
     public void testTextBoxHexShown() {
+        openDialog();
         onView(withId(R.id.textView_hex_color)).inRoot(isDialog()).check(matches(isDisplayed()));
     }
 
     @Test
     public void testTextBoxHexShowsSeekbarValues()
     {
+        openDialog();
         onView(withId(R.id.color_picker_seekbar_red)).perform(setProgress(0));
         onView(withId(R.id.color_picker_seekbar_green)).perform(setProgress(100));
         onView(withId(R.id.color_picker_seekbar_blue)).perform(setProgress(255));
@@ -182,6 +172,7 @@ public class ColorPickerDialogTest {
     @Test
     public void testSeekbarShowsTextBoxHexValues()
     {
+        openDialog();
         onView(withId(R.id.textView_hex_color)).perform(setText("64FF32"));
 
         onView(withId(R.id.color_picker_seekbar_red)).check(matches(withProgress(100)));
@@ -212,7 +203,7 @@ public class ColorPickerDialogTest {
     // helper function to set value on edit text
     public static ViewAction setText(final String value){
         return new ViewAction() {
-            @SuppressWarnings("unchecked")
+
             @Override
             public Matcher<View> getConstraints() {
                 return ViewMatchers.isAssignableFrom(EditText.class);
@@ -260,6 +251,26 @@ public class ColorPickerDialogTest {
             }
         };
     }
-    */
+
+    public void openDialog()
+    {
+        onView(withId(R.id.toolFlyoutMenu)).perform(click());
+        activityTestRule.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                int amount_items = activityTestRule.getActivity().toolFlyoutMenu.getAdapter().getCount();
+                for(int counter = 0; 0 < amount_items; counter++)
+                {
+                    FlyoutMenuView.MenuItem result = activityTestRule.getActivity().toolFlyoutMenu.getAdapter().getItem(counter);
+                    if(((FlyoutToolbar.MenuItemImage)result).getID() == R.drawable.ic_outline_color_lens_24px)
+                    {
+                        activityTestRule.getActivity().toolFlyoutMenu.setSelectedMenuItem(result);
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
 
 }
