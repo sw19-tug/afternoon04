af264b6982d53df9774103d3c0641704debbb95a
robgjo <robert.gjorgjiev@student.tugraz.at>
2019-05-28 19:30:42 +0200

    PAINT_010 added test but test fails [GR, UH]

---
+++ app/src/androidTest/java/at/tugraz/ist/swe/ImportImageTest.java
@@ -0,0 +1,160 @@
+package at.tugraz.ist.swe;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.MotionEvents;
+import android.support.test.rule.ActivityTestRule;
+import android.view.MotionEvent;
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.zakariya.flyoutmenu.FlyoutMenuView;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class ImportImageTest {
+
+    Context context;
+    InputStream ims = null;
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testDrawboardVisible()
+    {
+        onView(withId(R.id.main_canvas_view)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testOpenImage()
+    {
+        openPicture();
+    }
+
+    public void ImportImageTest(Context context)
+    {
+        this.context = context;
+
+    }
+    static{
+        System.loadLibrary("tensorflow_inference");
+    }
+    public void openPicture() {
+        try {
+            ims = context.getAssets().open("demo_img.png");
+            Bitmap bitmap = null;
+
+            bitmap = BitmapFactory.decodeStream(ims);
+        }
+        catch (IOException e){
+
+        }
+
+    }
+
+
+    public static ViewAction performTouch(final float x, final float y) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isDisplayed();
+            }
+
+            @Override
+            public String getDescription() {
+                return "perform touch down";
+            }
+
+            @Override
+            public void perform(UiController uiController, final View view) {
+
+                int[] location = new int[2];
+                view.getLocationOnScreen(location);
+
+                float[] coordinates = new float[] { x + location[0], y + location[1] };
+                float[] precision = new float[] { 1f, 1f };
+
+                MotionEvent down = MotionEvents.sendDown(uiController, coordinates, precision).down; // go down
+                uiController.loopMainThreadForAtLeast(200); // wait
+                MotionEvents.sendUp(uiController, down, coordinates); // go up
+                uiController.loopMainThreadForAtLeast(1000); // wait
+            }
+        };
+    }
+
+    public void openDialog()
+    {
+        onView(withId(R.id.toolFlyoutMenu)).perform(click());
+        activityTestRule.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                int amount_items = activityTestRule.getActivity().toolFlyoutMenu.getAdapter().getCount();
+                for(int counter = 0; 0 < amount_items; counter++)
+                {
+                    FlyoutMenuView.MenuItem result = activityTestRule.getActivity().toolFlyoutMenu.getAdapter().getItem(counter);
+                    if(((FlyoutToolbar.MenuItemImage)result).getID() == R.drawable.ic_outline_add_photo_alternate_24px)
+                    {
+                        activityTestRule.getActivity().toolFlyoutMenu.setSelectedMenuItem(result);
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    public static Matcher<View> checkCoordinates(final float x, final float y)
+    {
+        return new Matcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+
+            }
+
+            @Override
+            public boolean matches(Object item) {
+                DrawArea temp = (DrawArea) item;
+                Bitmap tool_bitmap = temp.getBitmap();
+
+                int alpha_color = Color.alpha(tool_bitmap.getPixel((int )x, (int) y));
+                int red_color = Color.red(tool_bitmap.getPixel((int) x,(int) y));
+                int green_color = Color.green(tool_bitmap.getPixel((int) x,(int) y));
+                int blue_color = Color.blue(tool_bitmap.getPixel((int) x,(int) y));
+
+                if(Color.BLACK == Color.argb(alpha_color, red_color, green_color, blue_color))
+                    return true;
+
+
+                return false;
+
+            }
+
+            @Override
+            public void describeMismatch(Object item, Description mismatchDescription) {
+            }
+
+            @Override
+            public void _dont_implement_Matcher___instead_extend_BaseMatcher_() {
+
+            }
+        };
+
+    }
+}
