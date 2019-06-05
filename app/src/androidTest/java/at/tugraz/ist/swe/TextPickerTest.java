22ada2e22c65539c640182bf7f8aa4b1339a5060
Anel <anel.memisevic@student.tugraz.at>
2019-06-05 16:07:24 +0200

    PAINT_014 added test to check if buttons are shown and have the correct color [MA, PA]

--- app/src/androidTest/java/at/tugraz/ist/swe/TextPickerTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/TextPickerTest.java
@@ -1,17 +1,7 @@
 package at.tugraz.ist.swe;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.widget.SeekBar;
-import android.support.test.espresso.ViewAction;
-import android.support.test.espresso.UiController;
-import android.support.test.espresso.matcher.BoundedMatcher;
-import android.widget.EditText;
-import android.view.View;
-import org.hamcrest.Matcher;
-import org.hamcrest.Description;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,17 +10,12 @@ import org.zakariya.flyoutmenu.FlyoutMenuView;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
-import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.RootMatchers.isDialog;
 import static android.support.test.espresso.matcher.ViewMatchers.hasTextColor;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
-
-import android.support.test.espresso.matcher.ViewMatchers;
 
 @RunWith(AndroidJUnit4.class)
 public class TextPickerTest {
@@ -44,6 +29,25 @@ public class TextPickerTest {
     {
         openDialog();
         onView(withText(R.string.text_picker_title)).check(matches(isDisplayed()));
+    }
+
+
+    @Test
+    public void testDialogButtonsVisible()
+    {
+        openDialog();
+        onView(withText(R.string.text_picker_button_apply)).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(withText(R.string.text_picker_button_cancel)).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(withText(R.string.text_picker_button_reset)).inRoot(isDialog()).check(matches(isDisplayed()));
+    }
+
+
+    @Test
+    public void testButtonColors(){
+        openDialog();
+        onView(withText("Cancel")).inRoot(isDialog()).check(matches(hasTextColor(R.color.colorCancelButtons)));
+        onView(withText("Reset")).inRoot(isDialog()).check(matches(hasTextColor(R.color.colorResetButtons)));
+        onView(withText("Apply")).inRoot(isDialog()).check(matches(hasTextColor(R.color.colorAcceptButtons)));
     }
 
     public void openDialog()
