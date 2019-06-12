2be1f3b022e0cbd2b6bdab58cb9546bd4b13438e
Anel <anel.memisevic@student.tugraz.at>
2019-06-12 16:07:56 +0200

    PAINT_017 added test to check the undo functionality [MA, PA]

--- app/src/androidTest/java/at/tugraz/ist/swe/RedoUndoTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/RedoUndoTest.java
@@ -1,8 +1,16 @@
 package at.tugraz.ist.swe;
 
+import android.graphics.Bitmap;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.MotionEvents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.MotionEvent;
+import android.view.View;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,7 +31,7 @@ public class RedoUndoTest {
     @Test
     public void testSelectedTool()
     {
-        openDialog();
+        openDialog(R.drawable.ic_baseline_swap_horiz_24px);
         onView(withId(R.id.strokeWidthLayout)).check(doesNotExist());
         onView(withId(R.id.strokewidth_left)).check(doesNotExist());
         onView(withId(R.id.strokewidth_right)).check(doesNotExist());
@@ -33,7 +41,20 @@ public class RedoUndoTest {
         onView(withId(R.id.buttonRedo)).check(matches(isDisplayed()));
     }
 
-    public void openDialog()
+    @Test
+    public void testUndoFunction()
+    {
+        Bitmap oldBitmap = (Bitmap)activityTestRule.getActivity().drawingArea.getBitmap();
+        openDialog(R.drawable.ic_si_glyph_circle);
+        onView(withId(R.id.main_canvas_view)).perform(performTouch(150, 150)); // click to close menu
+        onView(withId(R.id.main_canvas_view)).perform(performTouch(50, 50)); // click to draw
+        openDialog(R.drawable.ic_baseline_swap_horiz_24px);
+        onView(withId(R.id.buttonUndo)).perform(click());
+        Bitmap newBitmap = (Bitmap)activityTestRule.getActivity().drawingArea.getBitmap();
+        onView(withId(R.id.main_canvas_view)).check(matches(checkBitmap(oldBitmap,newBitmap)));
+    }
+
+    public void openDialog(final int tool)
     {
         onView(withId(R.id.toolFlyoutMenu)).perform(click());
         activityTestRule.getActivity().runOnUiThread(new Runnable() {
@@ -43,7 +64,7 @@ public class RedoUndoTest {
                 for(int counter = 0; 0 < amount_items; counter++)
                 {
                     FlyoutMenuView.MenuItem result = activityTestRule.getActivity().toolFlyoutMenu.getAdapter().getItem(counter);
-                    if(((FlyoutToolbar.MenuItemImage)result).getID() == R.drawable.ic_baseline_swap_horiz_24px)
+                    if(((FlyoutToolbar.MenuItemImage)result).getID() == tool)
                     {
                         activityTestRule.getActivity().toolFlyoutMenu.setSelectedMenuItem(result);
                         break;
@@ -51,6 +72,69 @@ public class RedoUndoTest {
                 }
             }
         });
+    }
+
+    public static ViewAction performTouch(final float x, final float y) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isDisplayed();
+            }
+
+            @Override
+            public String getDescription() {
+                return "perform touch down";
+            }
+
+            @Override
+            public void perform(UiController uiController, final View view) {
+
+                int[] location = new int[2];
+                view.getLocationOnScreen(location);
+
+                float[] coordinates = new float[] { x + location[0], y + location[1] };
+                float[] precision = new float[] { 1f, 1f };
+
+                MotionEvent down = MotionEvents.sendDown(uiController, coordinates, precision).down; // go down
+                uiController.loopMainThreadForAtLeast(200); // wait
+                MotionEvents.sendUp(uiController, down, coordinates); // go up
+                uiController.loopMainThreadForAtLeast(1000); // wait
+            }
+        };
+    }
+    public static Matcher<View> checkBitmap(final Bitmap oldBitmap, final Bitmap newBitmap)
+    {
+        return new Matcher<View>() {
+            @Override
+            public boolean matches(Object item) {
+                for(int x = 1; x < newBitmap.getWidth(); x++)
+                {
+                    for(int y = 1; y < newBitmap.getHeight(); y++)
+                    {
+                        if(newBitmap.getPixel(x, y) != oldBitmap.getPixel(x, y))
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+
+            @Override
+            public void describeMismatch(Object item, Description mismatchDescription) {
+
+            }
+
+            @Override
+            public void _dont_implement_Matcher___instead_extend_BaseMatcher_() {
+
+            }
+
+            @Override
+            public void describeTo(Description description) {
+
+            }
+        };
     }
 
 }
